0a98eb30178416139b6b88b4a0bbb11c8c29b6e6
Codwerk <louisws@outlook.fr>
2025-09-23 17:07:22 +0200

    afficher les réponses pour la question en cours

---
+++ app/components/Question.vue
@@ -0,0 +1,49 @@
+<script setup>
+  const props = defineProps({
+    question: {
+      type: Object,
+      required: true
+    }
+  })
+
+  const { question } = toRefs(props)
+
+  const emit = defineEmits(['answer'])
+
+  const answer = ref(null)
+</script>
+
+<template>
+    <div class="question">
+        <h3>{{ question.question }}</h3>
+        <ul>
+            <li 
+                v-for="(choice, i) in question.choices" 
+                :key="choice">
+
+                <label :for="`choice-${i}`">
+                    <input type="radio" 
+                        :id="`choice-${i}`" 
+                        :name="question.question"
+                        v-model="answer" 
+                        :value="choice" />
+                    {{ choice }}
+                </label>
+
+            </li>
+        </ul>
+        {{ answer }}
+        <button class="btn btn-primary">Suivant</button>
+    </div>
+</template>
+
+<style>
+  .question {
+    padding-top: 2rem;
+  }
+
+  .question button {
+    margin-left: auto;
+    display: block;
+  }
+</style>
--- app/components/Quizz.vue
+++ app/components/Quizz.vue
@@ -9,13 +9,11 @@
   const step = ref(0)
 
   const { quizz } = toRefs(props)
+  const currentQuestion = computed(() => quizz.value.questions[step.value])
 </script>
 
 <template>
     <h1>{{ quizz.title }}</h1>
     <Progress :value="step" :max="quizz.questions.length - 1" />
+    <Question :question="currentQuestion" v-if="currentQuestion" />
 </template>
-
-<style lang="scss" scoped>
-  
-</style>
